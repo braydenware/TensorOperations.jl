3bae7675309c2b30cbda8821b4fbe1f921edd0a0
Brayden Ware <brayden@physics.ucsb.edu>
2017-04-29 15:40:15 -0700

    fixed tests for change to non-modifying tensor operations

--- test/methods.jl
+++ test/methods.jl
@@ -4,7 +4,7 @@
 A=randn((3,5,4,6))
 p=randperm(4)
 C1=permutedims(A,p)
-C2=tensorcopy(A,1:4,p)
+C2,=tensorcopy(A,1:4,p)
 @test vecnorm(C1-C2)<eps()*sqrt(length(C1))*vecnorm(C1+C2)
 @test_throws TensorOperations.IndexError tensorcopy(A,1:3,1:4)
 @test_throws TensorOperations.IndexError tensorcopy(A,[1,2,2,4],1:4)
@@ -12,14 +12,14 @@ C2=tensorcopy(A,1:4,p)
 # test tensoradd
 B=randn((5,6,3,4))
 p=[3,1,4,2]
-C1=tensoradd(A,p,B,1:4)
+C1,=tensoradd(A,p,B,1:4)
 C2=A+permutedims(B,p)
 @test vecnorm(C1-C2)<eps()*sqrt(length(C1))*vecnorm(C1+C2)
 @test_throws DimensionMismatch tensoradd(A,1:4,B,1:4)
 
 # test tensortrace
 A=randn(50,100,100)
-C1=tensortrace(A,[:a,:b,:b])
+C1,=tensortrace(A,[:a,:b,:b])
 C2=zeros(50)
 for i=1:50
     for j=1:100
@@ -28,7 +28,7 @@ for i=1:50
 end
 @test vecnorm(C1-C2)<eps()*sqrt(length(C1))*vecnorm(C1+C2)
 A=randn(3,20,5,3,20,4,5)
-C1=tensortrace(A,[:a,:b,:c,:d,:b,:e,:c],[:e,:a,:d])
+C1,=tensortrace(A,[:a,:b,:c,:d,:b,:e,:c],[:e,:a,:d])
 C2=zeros(4,3,3)
 for i1=1:4, i2=1:3, i3=1:3
     for j1=1:20,j2=1:5
@@ -40,8 +40,8 @@ end
 # test tensorcontract
 A=randn(3,20,5,3,4)
 B=randn(5,6,20,3)
-C1=tensorcontract(A,[:a,:b,:c,:d,:e],B,[:c,:f,:b,:g],[:a,:g,:e,:d,:f];method=:BLAS)
-C2=tensorcontract(A,[:a,:b,:c,:d,:e],B,[:c,:f,:b,:g],[:a,:g,:e,:d,:f];method=:native)
+C1,=tensorcontract(A,[:a,:b,:c,:d,:e],B,[:c,:f,:b,:g],[:a,:g,:e,:d,:f];method=:BLAS)
+C2,=tensorcontract(A,[:a,:b,:c,:d,:e],B,[:c,:f,:b,:g],[:a,:g,:e,:d,:f];method=:native)
 C3=zeros(3,3,4,3,6)
 for a=1:3, b=1:20, c=1:5, d=1:3, e=1:4, f=1:6, g=1:3
     C3[a,g,e,d,f] += A[a,b,c,d,e]*B[c,f,b,g]
@@ -54,7 +54,8 @@ end
 # test tensorproduct
 A=randn(5,5,5,5)
 B=rand(Complex128,(5,5,5,5))
-C1=reshape(tensorproduct(A,[1,2,3,4],B,[5,6,7,8],[1,2,5,6,3,4,7,8]),(5*5*5*5,5*5*5*5))
+C1,=tensorproduct(A,[1,2,3,4],B,[5,6,7,8],[1,2,5,6,3,4,7,8])
+C1 =reshape(C1,(5*5*5*5,5*5*5*5))
 C2=kron(reshape(B,(25,25)),reshape(A,(25,25)))
 @test vecnorm(C1-C2)<eps()*sqrt(length(C1))*vecnorm(C1+C2)
 @test_throws TensorOperations.IndexError tensorproduct(A,[:a,:b,:c,:d],B,[:d,:e,:f,:g])
@@ -91,8 +92,8 @@ A=view(Abig,1+3*(0:9),2+2*(0:6),5+4*(0:6),4+3*(0:8))
 p=[3,1,4,2]
 Cbig=zeros(Complex128,(50,50,50,50))
 C=view(Cbig,13+(0:6),11+4*(0:9),15+4*(0:8),4+3*(0:6))
-Acopy=tensorcopy(A,1:4,1:4)
-Ccopy=tensorcopy(C,1:4,1:4)
+Acopy,=tensorcopy(A,1:4,1:4)
+Ccopy,=tensorcopy(C,1:4,1:4)
 TensorOperations.tensorcopy!(A,1:4,C,p)
 TensorOperations.tensorcopy!(Acopy,1:4,Ccopy,p)
 @test vecnorm(C-Ccopy)<eps()*sqrt(length(C))*vecnorm(C+Ccopy)
@@ -103,8 +104,8 @@ TensorOperations.tensorcopy!(Acopy,1:4,Ccopy,p)
 # tensoradd!
 Cbig=zeros(Complex128,(50,50,50,50))
 C=view(Cbig,13+(0:6),11+4*(0:9),15+4*(0:8),4+3*(0:6))
-Acopy=tensorcopy(A,1:4,p)
-Ccopy=tensorcopy(C,1:4,1:4)
+Acopy,=tensorcopy(A,1:4,p)
+Ccopy,=tensorcopy(C,1:4,1:4)
 alpha=randn()
 beta=randn()
 TensorOperations.tensoradd!(alpha,A,1:4,beta,C,p)
@@ -119,8 +120,8 @@ Abig=rand((30,30,30,30))
 A=view(Abig,1+3*(0:8),2+2*(0:14),5+4*(0:6),7+2*(0:8))
 Bbig=rand(Complex128,(50,50))
 B=view(Bbig,13+(0:14),3+5*(0:6))
-Acopy=tensorcopy(A,1:4)
-Bcopy=tensorcopy(B,1:2)
+Acopy,=tensorcopy(A,1:4)
+Bcopy,=tensorcopy(B,1:2)
 alpha=randn()
 beta=randn()
 TensorOperations.tensortrace!(alpha,A,[:a,:b,:c,:a],beta,B,[:b,:c])
@@ -141,9 +142,9 @@ Bbig=rand(Complex128,(50,50,50))
 B=view(Bbig,3+5*(0:6),7+2*(0:7),13+(0:14))
 Cbig=rand(Complex64,(40,40,40))
 C=view(Cbig,3+2*(0:8),13+(0:8),7+3*(0:7))
-Acopy=tensorcopy(A,1:4)
-Bcopy=tensorcopy(B,1:3)
-Ccopy=tensorcopy(C,1:3)
+Acopy,=tensorcopy(A,1:4)
+Bcopy,=tensorcopy(B,1:3)
+Ccopy,=tensorcopy(C,1:3)
 alpha=randn()
 beta=randn()
 Ccopy=beta*Ccopy
@@ -156,7 +157,7 @@ TensorOperations.tensorcontract!(alpha,A,[:a,:b,:c,:d],'N',B,[:c,:e,:b],'C',beta
 @test vecnorm(C-Ccopy)<eps(Float32)*vecnorm(C+Ccopy)*sqrt(length(C))
 Cbig=rand(Complex64,(40,40,40))
 C=view(Cbig,3+2*(0:8),13+(0:8),7+3*(0:7))
-Ccopy=tensorcopy(C,1:3)
+Ccopy,=tensorcopy(C,1:3)
 Ccopy=beta*Ccopy
 for d=1+(0:8),a=1+(0:8),e=1+(0:7)
     for b=1+(0:14),c=1+(0:6)
